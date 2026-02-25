7b06e20d50e561a3ef5174c203ad80afffdacd2a
francogcn <88166836+francogcn@users.noreply.github.com>
2026-02-25 16:34:58 -0300

    Comment clarification
    Added user input for peso amount and conversion output.

--- currency_convertion.py
+++ currency_convertion.py
@@ -1,8 +1,13 @@
 #! python3
+
+#Importacion de modulos
 import dolarhoy
 
+#Se pide al usuario ingresar el monto a convertir
 moneda = int(input("Ingrese el monto en pesos: "))
 
+#El valor de la cotizacion se obtiene mediante un fetch del modulo dolarhoy
 cotizacion = dolarhoy.get_dolarhoy()
 
+#Se imprime el monto convertido
 print(f"AR ${moneda} son U$D {moneda*cotizacion}")
