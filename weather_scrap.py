be3a30c2dd5e7dde79bad001985cb3d4df5f3b36
francogcn <88166836+francogcn@users.noreply.github.com>
2026-02-26 17:56:48 -0300

    Include feels like temperature in weather output
    Added functionality to display 'feels like' temperature alongside current temperature.

--- weather_scrap.py
+++ weather_scrap.py
@@ -2,15 +2,23 @@
 import requests, bs4
 #Download content from weather.com for city 'Posadas, Misiones'
 res = requests.get('https://weather.com/es-AR/tiempo/hoy/l/2d06ed300257681d1441a7014c3f72deb9e6073a1a998679ee383c23849ba0e5')
+
 #Program halts if download fails
 res.raise_for_status()
+
 #Parses the HTML download
 soup = bs4.BeautifulSoup(res.text,"html.parser")
+
 #Searches for the element with html class "CurrentConditions--tempValue--zUBSz"
 temperature  = soup.select('.CurrentConditions--tempValue--zUBSz')
-#If element is found, prints message with current temperature
-if len(temperature) > 0:
+
+#Searches for Feels
+feels_like = soup.select('.TodayDetailsCard--feelsLikeTempValue--8WgHV')
+
+#If elements are found, prints message with current temperature and what it feels like
+if len(temperature) > 0 and len(feels_like) > 0:
   print(f"Temperature in Posadas is {temperature[0].getText()}C")
+  print(f"Feels like: {fells_like[0].getText()}C")
   print("Source: Weather.com")
 else:
   print("Search failed")
