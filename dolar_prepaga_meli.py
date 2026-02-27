5bae45ba7c88ebbc0efd98ba381e31889fe4ecf2
francogcn <88166836+francogcn@users.noreply.github.com>
2026-02-27 15:27:01 -0300

    Refactor variable names for clarity

--- dolar_prepaga_meli.py
+++ dolar_prepaga_meli.py
@@ -1,12 +1,16 @@
+#Import required modules
 import requests, bs4
-
+#Declare headers to access page as user
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
+#Send HTTP request
 res = requests.get("https://www.mercadopago.com.ar/cards/commons/dollar-exchange", headers=headers)
+#Program halts if doonload fails
 res.raise_for_status()
 # Parses the content if the request succeeds
 soup = bs4.BeautifulSoup(res.text, "html.parser")
-dolar_prepaga = soup.select('.andes-money-amount')
-for dolar in dolar_prepaga:
+mep_dolar = soup.select('.andes-money-amount')
+#Prints the results for MEP Dolar
+for dolar in mep_dolar:
   print(dolar.getText())
