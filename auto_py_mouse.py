ddbb87e9fd726853435652386704bae2e6ace1ee
francogcn <88166836+francogcn@users.noreply.github.com>
2026-03-02 12:05:09 -0300

    Add files via upload

---
+++ auto_py_mouse.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+import random 
+# Get screen size to define boundaries for random movement
+#screen_width, screen_height = pyautogui.size()
+try:
+    while True:
+        # Generate random coordinates within screen boundaries
+        x = random.randint(50, 1200)
+        y = random.randint(200, 800)
+
+        # Move to the random coordinates with a short duration
+        pyautogui.moveTo(x, y, duration=0.25)
+        pyautogui.click(x,y)
+        pyautogui.vscroll(-10)
+        # Wait for 15 seconds before the next move
+        time.sleep(15)
+except pyautogui.FailSafeException:
+    print("Script terminated by user (mouse moved to corner).")
+except KeyboardInterrupt:
+    print("Script terminated by user (Ctrl+C).")
