9a20c9da62699f00c10913bd9ad6ec14db28e012
francogcn <88166836+francogcn@users.noreply.github.com>
2026-03-23 16:52:40 -0300

    Update play5_price.py

--- play5_price.py
+++ play5_price.py
@@ -1,13 +1,26 @@
+#Playstation 5 price scrapper
+#URL corresponds to a recomendations page in ML
 url='https://www.mercadolibre.com.ar/recommendations?client=pdp-seller_items-above&limit=20&site_platform=&web_device=desktop&min_recomms=3&bbw_ads=false&include_only_flex=false&site_id=MLA&category_id=MLA438566&item_id=MLA1696108475&product_id=MLA63094449&d2_id=e4366686-d10a-415c-b8aa-2ff616fd5407&use_polycard_order=true&user-zip-code=3300&user-platform=desktop&user-encoded-filters=deal%253AMLA1544201-2&buyer_location=&ads_vpp_tracking_id=bae15642-8a15-4c21-853b-52b1e28dc9dd'
+#Request is made via GET Method, using a header
 req = requests.get(url, headers=HEADERS)
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
+#Response is converted to JSON
 data = req.json()
 
+#Gets the product name from the JSON
 nombre_producto= data['recommendation_info']['polycards'][1]['components'][0]['title']['text']
-previous_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['previous_price']['value']
-current_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['current_price']['value']
-print(nombre_producto)
-print(previous_price)
-print(current_price)
+
+#Checks if the product is the requested PS5
+if nombre_producto == 'Sony Playstation 5 Consola Slim Standard + Juego Astro Bot Y Gt7 Blanco':
+  previous_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['previous_price']['value']
+  current_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['current_price']['value']
+  print(nombre_producto)
+  print('Previous price: AR$', previous_price)
+  print('Current price: AR$', current_price)
+
+#Shows error message in case is not the right product
+else:
+  print("Product not found")
+  print(f'found{nombre_producto} instead')
