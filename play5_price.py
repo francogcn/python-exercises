3298ab200022769f519c714b290f7309f342d48b
francogcn <88166836+francogcn@users.noreply.github.com>
2026-03-23 16:43:17 -0300

    Create play5_price.py

---
+++ play5_price.py
@@ -0,0 +1,13 @@
+url='https://www.mercadolibre.com.ar/recommendations?client=pdp-seller_items-above&limit=20&site_platform=&web_device=desktop&min_recomms=3&bbw_ads=false&include_only_flex=false&site_id=MLA&category_id=MLA438566&item_id=MLA1696108475&product_id=MLA63094449&d2_id=e4366686-d10a-415c-b8aa-2ff616fd5407&use_polycard_order=true&user-zip-code=3300&user-platform=desktop&user-encoded-filters=deal%253AMLA1544201-2&buyer_location=&ads_vpp_tracking_id=bae15642-8a15-4c21-853b-52b1e28dc9dd'
+req = requests.get(url, headers=HEADERS)
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+data = req.json()
+
+nombre_producto= data['recommendation_info']['polycards'][1]['components'][0]['title']['text']
+previous_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['previous_price']['value']
+current_price = data['recommendation_info']['polycards'][1]['components'][1]['price']['current_price']['value']
+print(nombre_producto)
+print(previous_price)
+print(current_price)
